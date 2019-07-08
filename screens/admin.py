52e66535d8749eb206de8fa693c6c973a99015c3
Kyle Cooke <cookekyle99@yahoo.co.uk>
2019-07-08 19:09:12 +0100

    display schedule nicer

--- screens/admin.py
+++ screens/admin.py
@@ -16,8 +16,20 @@ class PlaylistDisplay(admin.ModelAdmin):
     inlines = [PlaylistEntryInline]
 
 
+class ScheduleRuleInline(admin.StackedInline):
+    model = ScheduleRule
+    extra = 1
+
+
+class ScheduleDisplay(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name', 'description', 'default_playlist', 'is_default']}),
+    ]
+    inlines = [ScheduleRuleInline]
+
+
 admin.site.register(Playlist, PlaylistDisplay)
-admin.site.register(Schedule)
+admin.site.register(Schedule, ScheduleDisplay)
 admin.site.register(ScheduleRule)
 admin.site.register(Screen)
 admin.site.register(Source)
