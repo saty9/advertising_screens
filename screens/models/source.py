4cc6a8a8c13b8db2815371d24c0143bd9309ff39
kyle <cookekyle97@gmail.com>
2019-08-28 12:02:04 +0100

    added help text and changed display name of iframe option

--- screens/models/source.py
+++ screens/models/source.py
@@ -17,14 +17,15 @@ class Source(models.Model):
     types = (
         (IMAGE, 'Image'),
         (VIDEO, 'Video'),
-        (IFRAME, 'IFrame'),
+        (IFRAME, 'Website'),
     )
     type = models.CharField(max_length=3, choices=types)
     name = models.TextField()
     file = models.FileField(upload_to=get_file_path,
                             null=True,
-                            blank=True)
-    url = models.URLField(blank=True)
+                            blank=True,
+                            help_text="resolution of files should be 1360x768, videos must be mp4")
+    url = models.URLField(blank=True, verbose_name="Website Address", help_text="only required if website type")
     exclude_from_play_all = models.BooleanField(default=False)
     expires_at = models.DateTimeField(blank=True, null=True, default=None)
 
