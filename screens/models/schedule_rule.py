4809b902b0530f0505ccc83186a59100e0111854
Kyle Cooke <kyle@playerdata.com>
2021-08-28 11:53:06 +0100

    change before save to offset matching start and end times by 1 second

--- screens/models/schedule_rule.py
+++ screens/models/schedule_rule.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+from datetime import timedelta, datetime
 
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import Signal, receiver
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 from django.db import models
@@ -16,8 +16,8 @@ class ScheduleRule(models.Model):
     schedule = models.ForeignKey(to=Schedule, on_delete=models.CASCADE)
     starts = models.DateField()
     occurrences = RecurrenceField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    start_time = models.TimeField(help_text="Matching start and end times will create the end time 1 second before the start time")
+    end_time = models.TimeField(help_text="End times before the start time will wrap around into the day after")
     priority = models.IntegerField()
 
     def is_expired(self):
@@ -26,5 +26,6 @@ class ScheduleRule(models.Model):
 
 @receiver(pre_save, sender=ScheduleRule)
 def before_save(sender, **kwargs):
-    if kwargs['instance'].end_time == datetime.min.time():
-        kwargs['instance'].end_time = datetime.max.time()
+    if kwargs['instance'].start_time == kwargs['instance'].end_time:
+        end_time = datetime.combine(timezone.now().date(), kwargs['instance'].end_time) - timedelta(seconds=1)
+        kwargs['instance'].end_time = end_time.time()
