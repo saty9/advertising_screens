4f65fcdf63a80444d18fc77177ac74ae9a86ec50
dunnmj <dunnmj@users.noreply.github.com>
2026-03-07 18:41:54 +0000

    feat: enhance admin displays with additional fields and filters for Playlist, Schedule, and Source models

--- screens/admin.py
+++ screens/admin.py
@@ -1,7 +1,9 @@
 from admin_ordering.admin import OrderableAdmin
 from django.urls import re_path
 from django.contrib import admin
+from django.template.response import TemplateResponse
 from unfold.admin import ModelAdmin, TabularInline, StackedInline
+from unfold.decorators import display
 
 from screens.forms import SourceBulkCreateForm, PlaylistAssigningSourceForm
 from screens.models import Playlist, Schedule, ScheduleRule, Screen, Source, PlaylistEntry
@@ -10,6 +12,8 @@ from screens.models import Playlist, Schedule, ScheduleRule, Screen, Source, Pla
 class PlaylistEntryInline(OrderableAdmin, TabularInline):
     model = PlaylistEntry
     ordering_field = 'number'
+    extra = 0
+    fields = ('number', 'source', 'duration')
 
 
 class PlaylistParentsInline(TabularInline):
@@ -17,14 +21,23 @@ class PlaylistParentsInline(TabularInline):
     fk_name = "inheriting_list"
     verbose_name_plural = "Playlists to inherit from"
     verbose_name = "Parent List"
-    extra = 1
+    extra = 0
 
 
+@admin.register(Playlist)
 class PlaylistDisplay(ModelAdmin):
+    list_display = ('name', 'show_source_count', 'plays_everything', 'last_updated')
+    search_fields = ('name', 'description')
+    list_filter = ('plays_everything',)
+    readonly_fields = ('last_updated',)
     fieldsets = [
-        (None,               {'fields': ['name', 'description', 'interspersed_source', 'plays_everything']}),
+        (None, {'fields': ['name', 'description', 'plays_everything', 'interspersed_source', 'last_updated']}),
     ]
     inlines = [PlaylistParentsInline, PlaylistEntryInline]
+
+    @display(description="Sources")
+    def show_source_count(self, obj):
+        return obj.playlistentry_set.count()
 
     def get_urls(self):
         urls = super().get_urls()
@@ -42,14 +55,28 @@ class PlaylistDisplay(ModelAdmin):
 
 class ScheduleRuleInline(StackedInline):
     model = ScheduleRule
-    extra = 1
+    extra = 0
+    fields = ('playlist', 'starts', 'occurrences', 'start_time', 'end_time', 'priority')
 
 
+@admin.register(Schedule)
 class ScheduleDisplay(ModelAdmin):
+    list_display = ('name', 'default_playlist', 'is_default')
+    search_fields = ('name', 'description')
+    list_filter = ('is_default',)
+    list_select_related = ('default_playlist',)
     fieldsets = [
-        (None,               {'fields': ['name', 'description', 'default_playlist', 'is_default']}),
+        (None, {'fields': ['name', 'description', 'default_playlist', 'is_default']}),
     ]
     inlines = [ScheduleRuleInline]
+
+
+@admin.register(ScheduleRule)
+class ScheduleRuleAdmin(ModelAdmin):
+    list_display = ('schedule', 'playlist', 'starts', 'start_time', 'end_time', 'priority')
+    list_filter = ('schedule',)
+    list_select_related = ('schedule', 'playlist')
+    search_fields = ('schedule__name', 'playlist__name')
 
 
 class PlaylistListFilter(admin.SimpleListFilter):
@@ -60,27 +87,37 @@ class PlaylistListFilter(admin.SimpleListFilter):
         return Playlist.objects.values_list("id", "name")
 
     def queryset(self, request, queryset):
-        return queryset.filter(playlist__id=self.value()) or queryset.filter(playlistentry__playlist__id=self.value())
+        if self.value() is None:
+            return queryset
+        return queryset.filter(playlistentry__playlist__id=self.value())
 
 
+@admin.register(Source)
 class SourceDisplay(ModelAdmin):
     readonly_fields = ('image_preview',)
-    list_display = ('name', 'playlist_names', 'created_at', "valid_from", "expires_at")
-    list_filter = (
-        PlaylistListFilter,
-        "type",
-    )
+    list_display = ('name', 'show_type', 'resolution', 'created_by', 'playlist_names', 'created_at', 'valid_from', 'expires_at')
+    list_filter = (PlaylistListFilter, 'type')
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+
+    @display(description="Type", label={
+        "Image": "success",
+        "Video": "warning",
+        "Website": "info",
+    })
+    def show_type(self, obj):
+        return obj.get_type_display()
 
     def get_urls(self):
-        urls = super(SourceDisplay, self).get_urls()
+        urls = super().get_urls()
         my_urls = [
-            re_path(r'^bulk_create/$', self.bulk_create_view),
+            re_path(r'^bulk_create/$', self.bulk_create_view, name='screens_source_bulk_create'),
         ]
         return my_urls + urls
 
     def bulk_create_view(self, request):
         request.bulk_create = True
-        return super(SourceDisplay, self).changeform_view(request)
+        return super().changeform_view(request)
 
     def get_form(self, request, obj=None, **kwargs):
         if obj is None and hasattr(request, "bulk_create") and request.bulk_create:  # TODO and bulk create permission
@@ -103,14 +140,17 @@ class SourceDisplay(ModelAdmin):
         )
 
 
+@admin.register(Screen)
 class ScreenAdmin(ModelAdmin):
     readonly_fields = ('screen_preview',)
-    list_display = ('name', 'ip', 'online', 'last_seen')
+    list_display = ('name', 'ip', 'show_online', 'last_seen', 'schedule')
+    search_fields = ('name', 'ip')
+    list_filter = ('schedule',)
+    list_select_related = ('schedule',)
+
+    @display(description="Online", boolean=True)
+    def show_online(self, obj):
+        return obj.online()
 
 
-admin.site.register(Playlist, PlaylistDisplay)
-admin.site.register(Schedule, ScheduleDisplay)
-admin.site.register(ScheduleRule)
-admin.site.register(Screen, ScreenAdmin)
-admin.site.register(Source, SourceDisplay)
 admin.site.site_header = "Display Screen Admin"
